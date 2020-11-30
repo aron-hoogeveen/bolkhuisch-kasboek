20548299ab979be4c8290b5f872e99602ba1b544
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2020-11-30 18:36:54 +0100

    Add MenuBar

--- src/main/java/ch/bolkhuis/kasboek/ApplicationSceneRoot.java
+++ src/main/java/ch/bolkhuis/kasboek/ApplicationSceneRoot.java
@@ -2,10 +2,12 @@ package ch.bolkhuis.kasboek;
 
 import ch.bolkhuis.kasboek.components.AccountingEntityTreeTableView;
 import ch.bolkhuis.kasboek.core.HuischLedger;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.events.Event;
 
 import java.io.File;
 
@@ -43,6 +45,75 @@ public class ApplicationSceneRoot extends BorderPane {
     }
 
     private void createAndSetChildren() {
+        createAndSetTopMenuBar();
+        createAndSetCenterTabPane();
+
+
+    }
+
+    /**
+     * Creates and sets the MenuBar in the top of the window.
+     */
+    private void createAndSetTopMenuBar() {
+        // File menu
+        Menu fileMenu = new Menu("Bestand");
+        MenuItem newFile = new MenuItem("Nieuwe maken");
+//        newFile.setOnAction(new NewFileEventHandler());
+        MenuItem openFile = new MenuItem("Openen");
+//        openFile.setOnAction(new OpenFileEventHandler());
+        MenuItem closeFile = new MenuItem("Sluiten");
+//        closeFile.setOnAction(new CloseFileEventHandler());
+        MenuItem saveFile = new MenuItem("Opslaan");
+//        saveFile.setOnAction(new SaveFileEventHandler());
+        MenuItem saveAsFile = new MenuItem("Opslaan als");
+//        saveAsFile.setOnAction(new SaveAsFileEventHandler());
+        fileMenu.getItems().addAll(
+                newFile,
+                openFile,
+                closeFile,
+                new SeparatorMenuItem(),
+                saveFile,
+                saveAsFile
+        );
+        // Edit menu
+        Menu editMenu = new Menu("Bewerken");
+        MenuItem addAccountingEntity = new MenuItem("Entiteit toevoegen");
+//        addAccountingEntity.setOnAction(new AddAccountingEntityEventHandler());
+        MenuItem addInmateEntity = new MenuItem("Huischgenoot toevoegen");
+//        addInmateEntity.setOnAction(new AddInmateEntityEventHandler());
+//        addAccountingEntity.setOnAction(new AddAccountingEntityEventHandler());
+        editMenu.getItems().addAll(
+                addAccountingEntity,
+                addInmateEntity
+        );
+        // Print menu
+        Menu toolsMenu = new Menu("Gereedschappen");
+        Menu printSubMenu = new Menu("Factuur Printen");
+        MenuItem printInvoicesForAllInmates = new MenuItem("Alle Huischgenoten");
+        MenuItem printInvoicesForSelectedInmates = new MenuItem("Selecteer Huischgenoten");
+        printSubMenu.getItems().addAll(
+                printInvoicesForAllInmates,
+                printInvoicesForSelectedInmates
+        );
+        toolsMenu.getItems().addAll(
+                printSubMenu
+        );
+
+        // Add the File Menus to the MenuBar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(
+                fileMenu,
+                editMenu,
+                toolsMenu
+        );
+
+        setTop(menuBar);
+    }
+
+    /**
+     * Creates and sets the tab pane showing the AccountingEntities, Receipts and Transactions
+     */
+    private void createAndSetCenterTabPane() {
         // The tabpane with the AccountingEntities, Receipts and Transactions
         TabPane tabPane = new TabPane();
         tabPane.setId("main-tab-pane");
