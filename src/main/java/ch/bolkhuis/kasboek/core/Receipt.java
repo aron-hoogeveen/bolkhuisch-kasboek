5c986f60931c3e3fec6ebe662b2541d7c725d08c
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2021-01-31 15:17:39 +0100

    Implement comparable

--- src/main/java/ch/bolkhuis/kasboek/core/Receipt.java
+++ src/main/java/ch/bolkhuis/kasboek/core/Receipt.java
@@ -30,7 +30,7 @@ import java.util.Set;
  *
  * TODO add field that indicates that the payer is already invoiced for this Receipt. If the field is true, than this Receipt cannot be edited
  */
-public class Receipt {
+public class Receipt implements Comparable<Receipt> {
     private final int id;
     private @NotNull final String name;
     private @NotNull final ObservableSet<Integer> transactionIdSet;
@@ -123,5 +123,68 @@ public class Receipt {
     @Override
     public String toString() {
         return name + " (" + date + ")";
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure
+     * {@code sgn(x.compareTo(y)) == -sgn(y.compareTo(x))}
+     * for all {@code x} and {@code y}.  (This
+     * implies that {@code x.compareTo(y)} must throw an exception iff
+     * {@code y.compareTo(x)} throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
+     * {@code x.compareTo(z) > 0}.
+     *
+     * <p>Finally, the implementor must ensure that {@code x.compareTo(y)==0}
+     * implies that {@code sgn(x.compareTo(z)) == sgn(y.compareTo(z))}, for
+     * all {@code z}.
+     *
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
+     * class that implements the {@code Comparable} interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     *
+     * <p>In the foregoing description, the notation
+     * {@code sgn(}<i>expression</i>{@code )} designates the mathematical
+     * <i>signum</i> function, which is defined to return one of {@code -1},
+     * {@code 0}, or {@code 1} according to whether the value of
+     * <i>expression</i> is negative, zero, or positive, respectively.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(@NotNull Receipt o) {
+        // date
+        int comp = this.date.compareTo(o.date);
+        if (comp != 0) {
+            return comp;
+        }
+
+        // payer
+        comp = this.payer - o.payer;
+        if (comp != 0) {
+            return comp;
+        }
+
+        // description
+        comp = this.name.compareTo(o.name);
+        if (comp != 0) {
+            return comp;
+        }
+
+        // id
+        return this.id - o.id;
     }
 }
